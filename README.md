e96fee7a87117394de338441f7fb317d2ecb1cda
Brian Turchyn <brian@dj-bri-t.net>
2015-05-29 12:08:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# lita-responses
+
+TODO: Add a description of the plugin.
+
+## Installation
+
+Add lita-responses to your Lita instance's Gemfile:
+
+``` ruby
+gem "lita-responses"
+```
+
+## Configuration
+
+TODO: Describe any configuration attributes the plugin exposes.
+
+## Usage
+
+TODO: Describe the plugin's features and how to use them.
---
+++ lib/lita/handlers/responses.rb
@@ -0,0 +1,75 @@
+module Lita
+  module Handlers
+    class Responses < Handler
+      on :loaded, :define_responses
+
+      def define_responses(payload)
+        puts "defining static routes"
+        define_static_routes
+        # define_dynamic_routes
+      end
+
+      def define_static_routes
+        self.class.route(
+          %r{^respond to "(.+?)" with "(.+?)"}i,
+          :add_response,
+          command: true
+        )
+
+        self.class.route(
+          %r{^stop responding to "(.+?)"$}i,
+          :remove_response,
+          command: true
+        )
+
+        self.class.route(
+          %r{^(.+?)$}i,
+          :lookup_response
+        )
+      end
+
+      def add_response(response)
+        puts "Adding response"
+        q = response.matches[0][0]
+        a = response.matches[0][1]
+
+        update_response q, a
+
+        response.reply_with_mention "You got it!"
+      end
+
+      def remove_response(response)
+        q = response.matches[0][0]
+
+        remove q
+
+        response.reply_with_mention "Consider it forgotten, cap'n!"
+      end
+
+      def update_response(question, answer)
+        redis.set full_key(question), answer
+      end
+
+      def remove(question)
+        if redis.exists(full_key(question))
+          redis.del(full_key(question))
+        end
+      end
+
+      def lookup_response(response)
+        q = response.matches[0][0]
+        answer = nil
+        if redis.exists(full_key(q))
+          answer = redis.get(full_key(q))
+        end
+        response.reply_with_mention answer if answer
+      end
+
+      def full_key(key)
+        "lita-responses:#{key.downcase}"
+      end
+    end
+
+    Lita.register_handler(Responses)
+  end
+end
